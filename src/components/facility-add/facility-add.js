ce776b9e91b11192528a99e2aae4a8f8fe51942f
yurimovo <yuri22091983@gmail.com>
2021-12-26 22:55:36 +0600

    facility add design

--- src/components/facility-add/facility-add.js
+++ src/components/facility-add/facility-add.js
@@ -18,7 +18,7 @@ export default class FacilityAdd extends Component {
             <div className="container facility-add">
                 <div className="row">
                     <div className="col-sm-12 text-center">
-                        <h2>Добавление объекта</h2>
+                        <h2 className="h2">Добавление объекта</h2>
                     </div>
                 </div>
                 <div className="row">
@@ -71,7 +71,7 @@ export default class FacilityAdd extends Component {
                     <div className="col-sm-4">
                         <div className="dropdown dd">
                             <button 
-                                className="btn btn-secondary dropdown-toggle" 
+                                className="btn btn-outline-primary dropdown-toggle button-dd btn-block" 
                                 type="button" 
                                 id="ownershipType" 
                                 data-toggle="dropdown" 
@@ -87,7 +87,7 @@ export default class FacilityAdd extends Component {
                     <div className="col-sm-4">
                         <div className="dropdown dd">
                             <button 
-                                className="btn btn-secondary dropdown-toggle" 
+                                className="btn btn-outline-primary dropdown-toggle button-dd btn-block" 
                                 type="button" 
                                 id="facilityCategory" 
                                 data-toggle="dropdown" 
@@ -105,7 +105,7 @@ export default class FacilityAdd extends Component {
                     <div className="col-sm-4">
                         <div className="dropdown dd">
                             <button 
-                                className="btn btn-secondary dropdown-toggle" 
+                                className="btn btn-outline-primary dropdown-toggle button-dd btn-block" 
                                 type="button" 
                                 id="securityType" 
                                 data-toggle="dropdown" 
@@ -139,8 +139,161 @@ export default class FacilityAdd extends Component {
                         <input 
                             type="text" 
                             className="form-control input" 
-                            placeholder="Объект" 
+                            placeholder="Номер литерного дела" 
                             id="letteredFileNumber"
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <div className="dropdown dd">
+                            <button 
+                                className="btn btn-outline-primary dropdown-toggle button-dd btn-block" 
+                                type="button" 
+                                id="spi" 
+                                data-toggle="dropdown" 
+                                aria-expanded="false">
+                                    Тип СПИ
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                <li><button className="dropdown-item" type="button">АСПИ Приток Ethernet</button></li>
+                                <li><button className="dropdown-item" type="button">АСПИ Приток GSM</button></li>
+                                <li><button className="dropdown-item" type="button">РСПИ Иртыш-3Р</button></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-sm-6">
+                        <div className="dropdown dd">
+                            <button 
+                                className="btn btn-outline-primary dropdown-toggle button-dd btn-block" 
+                                type="button" 
+                                id="facilityHardware" 
+                                data-toggle="dropdown" 
+                                aria-expanded="false">
+                                    Объектовый прибор
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                <li><button className="dropdown-item" type="button">БО Иртыш-112</button></li>
+                                <li><button className="dropdown-item" type="button">БО Иртыш-113</button></li>
+                                <li><button className="dropdown-item" type="button">БО Иртыш-214</button></li>
+                                <li><button className="dropdown-item" type="button">БО Иртыш-214-GSM</button></li>
+                                <li><button className="dropdown-item" type="button">Мобильный телохранитель</button></li>
+                                <li><button className="dropdown-item" type="button">ППКОП Приток 011-8-1-011(М)</button></li>
+                                <li><button className="dropdown-item" type="button">ППКОП Приток-А-КОП-02(03,04,05)</button></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <div className="dropdown dd">
+                            <button 
+                                className="btn btn-outline-primary dropdown-toggle button-dd btn-block" 
+                                type="button" 
+                                id="mountingOrganization" 
+                                data-toggle="dropdown" 
+                                aria-expanded="false">
+                                    Монтажная организация
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                <li><button className="dropdown-item" type="button">ООО Булгар</button></li>
+                                <li><button className="dropdown-item" type="button">ООО ЛенИС</button></li>
+                                <li><button className="dropdown-item" type="button">ООО ОСБ</button></li>
+                                <li><button className="dropdown-item" type="button">ООО Эгида</button></li>
+                                <li><button className="dropdown-item" type="button">ФГУП Охрана</button></li>
+                                <li><button className="dropdown-item" type="button">Отсутствует</button></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-sm-6">
+                        <div className="dropdown dd">
+                            <button 
+                                className="btn btn-outline-primary dropdown-toggle button-dd btn-block" 
+                                type="button" 
+                                id="survingOrganization" 
+                                data-toggle="dropdown" 
+                                aria-expanded="false">
+                                    Обслуживающая организация
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                <li><button className="dropdown-item" type="button">ООО Булгар</button></li>
+                                <li><button className="dropdown-item" type="button">ООО ЛенИС</button></li>
+                                <li><button className="dropdown-item" type="button">ООО ОСБ</button></li>
+                                <li><button className="dropdown-item" type="button">ООО Эгида</button></li>
+                                <li><button className="dropdown-item" type="button">ФГУП Охрана</button></li>
+                                <li><button className="dropdown-item" type="button">Отсутствует</button></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-4">
+                        <input 
+                            type="text" 
+                            className="form-control input" 
+                            placeholder="Пультовые номера" 
+                            id="pultNumbers"
+                        />
+                    </div>
+                    <div className="col-sm-4">
+                        <input 
+                            type="text" 
+                            className="form-control input" 
+                            placeholder="Номер телефофна СИМ" 
+                            id="simNumber"
+                        />
+                    </div>
+                    <div className="col-sm-4">
+                        <input 
+                            type="text" 
+                            className="form-control input" 
+                            placeholder="Заводской номер" 
+                            id="serialNumber"
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <input 
+                            type="text" 
+                            className="form-control input" 
+                            placeholder="FCC ID СИМ-карты" 
+                            id="fccID"
+                        />
+                    </div>
+                    <div className="col-sm-6">
+                        <div className="dropdown dd">
+                            <button 
+                                className="btn btn-outline-primary dropdown-toggle button-dd btn-block" 
+                                type="button" 
+                                id="responsible" 
+                                data-toggle="dropdown" 
+                                aria-expanded="false">
+                                    Ответственный
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                <li><button className="dropdown-item" type="button">Белик А.П.</button></li>
+                                <li><button className="dropdown-item" type="button">Величко А.С.</button></li>
+                                <li><button className="dropdown-item" type="button">Шешиков Ю.С.</button></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <input 
+                            type="text" 
+                            className="form-control input" 
+                            placeholder="Подбор объекта" 
+                            id="assortment"
+                        />
+                    </div>
+                    <div className="col-sm-6">
+                        <input 
+                            type="text" 
+                            className="form-control input" 
+                            placeholder="Часы охраны" 
+                            id="hours"
                         />
                     </div>
                 </div>
