295397e1e033cd50effe99e52893d1ea3f8ea15b
yurimovo <yuri22091983@gmail.com>
2022-01-09 22:18:19 +0600

    __1__

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -14,6 +14,8 @@ router.post(
     ],
     async(req, res) => {
     try {
+        console.log('Body:', req.body)
+
         const errors = validationResult(req)
 
         if (!errors.isEmpty()) {
